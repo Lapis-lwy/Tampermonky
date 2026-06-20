517aa1bb50b818e162e4b9f1bce1010d550d95e3
Lapis-lwy <lapis_lwy@foxmail.com>
2026-06-20 09:40:29 +0000

    67

--- PixivInfo.user.js
+++ PixivInfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PixivInfo
 // @namespace    http://tampermonkey.net/
-// @version      9.2
+// @version      9.3
 // @description  查看本地是否存在该图片
 // @author       Lapis_lwy
 // @match        *://www.pixiv.net/*
